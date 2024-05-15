77d7eab5eaec58410b00a4ce7e30f2722acb10b2
Juan Niebles <j.niebles@uniandes.edu.co>
2024-05-15 08:21:42 -0500

    crear entidades básicas

---
+++ src/app/pais/pais-list/pais-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-list',
+  templateUrl: './pais-list.component.html',
+  styleUrl: './pais-list.component.css'
+})
+export class PaisListComponent {
+
+}
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaisListComponent } from './pais-list/pais-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaisListComponent
+  ],
   imports: [
     CommonModule
   ]
